7dc5c06dac7e225b83788be4ab952fca6f578908
sangvu01 <vuminhsang01@gmail.com>
2026-07-16 16:28:40 +0700

    Define chapters' htmls

---
+++ Self_LMS/handbook/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base(req):
+    return render(req, 'base.html')
+
+def c1(req):
+    return render(req, 'docs/chap1.html',{
+        "page" : "c1"
+    })
+
+def c2(req):
+    return render(req, 'docs/chap2.html',{
+        "page" : "c2"
+    })
+
+def c3(req):
+    return render(req, 'docs/chap3.html',{
+        "page" : "c3"
+    })
+
+def c4(req):
+    return render(req, 'docs/chap4.html',{
+        "page" : "c4"
+    })
+
+def c5(req):
+    return render(req, 'docs/chap5.html',{
+        "page" : "c5"
+    })
+
+def c6(req):
+    return render(req, 'docs/chap6.html',{
+        "page" : "c6"
+    })
+
